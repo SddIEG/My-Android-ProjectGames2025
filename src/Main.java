54649a81d328d36b8404aa52a3277ec20fb18e0a
SddIEG <artempalcev3@gmail.com>
2025-09-23 19:31:57 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int stepa = 0;
+        int personazX, personaY;
+        int personaLife = 3;
+        int sizeBoard = 5;
+        System.out.println("------------------------");
+        System.out.println("| |");
+        System.out.println("| |");
+        System.out.println("------------------------");
+
+
+    }
+}
