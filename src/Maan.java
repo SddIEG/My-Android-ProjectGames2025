633b55feb2415966ea9fe0dc64f055873c1675cb
SddIEG <artempalcev3@gmail.com>
2025-10-14 19:15:14 +0300

    Update Maan.java

--- src/Maan.java
+++ src/Maan.java
@@ -1,20 +1,20 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Maan {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int stepa = 0;
         int difficult;
-String caslte = "\uD83C\uDFF0";
+        String caslte = "\uD83C\uDFF0";
 
         int sizeBoard = 5;
         int kolvoMonster = sizeBoard * sizeBoard - sizeBoard - 5;
 
         Random r = new Random();
-        int castleX = r.nextInt(sizeBoard);
-        int castleY = 0;
+        int castleX = 0;
+        int castleY = r.nextInt(sizeBoard);
         String[][] board = new String[sizeBoard][sizeBoard];
         for (int y = 0; y < sizeBoard; y++) {
             for (int x = 0; x < sizeBoard; x++) {
@@ -41,25 +41,37 @@ String caslte = "\uD83C\uDFF0";
 
         }
 
-        Main settings = new Main();
-        System.out.println("Привет! Ты готов начать играть в игру? (Напиши: ДА или НЕТ)");
+        System.out.println("Приветствую тебя!.Ты готов ?(Напиши Да/Нет)");
         String ans = scanner.nextLine();
 
 
         switch (ans) {
-            case "ДА" -> {
-                settings.configureSettings(scanner);
-                difficult = settings.getDifficulty();
+            case "Да" -> {
                 System.out.println("Начинаем играть)");
                 System.out.println("Выберите уровень сложности");
                 difficult = scanner.nextInt();
-                System.out.println("Ты выбрал уровень сложности:\t" + difficult);
+                if (difficult != 5) {
+                    System.out.println("Ты выбрал уровень сложности:\t" + difficult);
+
+                } else {
+                    System.out.println("你选择了难度级别吗:\t" + difficult);
+                }
+
                 while (true) {
                     board[persona.getY() - 1][persona.getX() - 1] = persona.getImage();
                     outputBoard(board, persona.getLive());
-                    System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;" +
-                            "\nКоординаты персонажа - (x: " + persona.getX() + ", y: " + persona.getY() + "))");
+                    if (difficult != 5) {
+                        System.out.println("Жду твоего хода\n" +
+                                "(Разрешаю ходить только по вертикали и горизонтали и только один раз)" +
+                                "\nТы тут - (x: " + persona.getX() + ", y: " + persona.getY() + "))");
+                    } else {
+                        System.out.println("等待你的移动\n" +
+                                "（我允许你只走垂直和水平，只有一次）" +
+                                "\n你在这儿 - (x: " + persona.getX() + ", y: " + persona.getY() + "))");
+                    }
+                    System.out.print("X ");
                     int x = scanner.nextInt();
+                    System.out.print("Y ");
                     int y = scanner.nextInt();
 
                     if (persona.moveCorrect(x, y)) {
@@ -68,11 +80,24 @@ String caslte = "\uD83C\uDFF0";
                             board[persona.getY() - 1][persona.getX() - 1] = "  ";
                             persona.move(x, y);
                             stepa++;
-                            System.out.println("Ход корректный; Новые координаты: " + persona.getX() + ", " + persona.getY() +
-                                    "\nХод номер: " + stepa);
+                            if (difficult != 5) {
+                                System.out.println("Я арзрешаю тебе туда ходить .Теперь ты тут: " + persona.getX() + ", " + persona.getY() +
+                                        "\nХод номер: " + stepa);
+                            } else {
+                                System.out.println("我禁止你去那里。你现在在这里: " + persona.getX() + ", " + persona.getY() +
+                                        "\n移动号码: " + stepa);
+                            }
+
                         } else if (next.equals(caslte)) {
-                            System.out.println("Вы прошли игру!");
-                            break;
+                            if (difficult != 5) {
+                                System.out.println("Крассавчик");
+                                break;
+                            } else {
+                                System.out.println("哈哈傻瓜");
+                                break;
+                            }
+
+
                         } else {
                             for (Monster monster : arrayMonster) {
                                 if (monster.conflictPerson(x, y)) {
@@ -88,34 +113,40 @@ String caslte = "\uD83C\uDFF0";
                             }
                         }
                     } else {
-                        System.out.println("Неккоректный ход");
+                        if (difficult != 5) {
+                            System.out.println("Низя");
+                        } else {
+                            System.out.println("不得");
+                        }
+
+
                     }
                 }
             }
-            case "НЕТ" -> System.out.println("Жаль, приходи еще!");
-            default -> System.out.println("Данные введены неккоректно");
+            case "Нет" -> System.out.println("Ну и УХОДИ:((");
+            default -> System.out.println("Не понимаю @_@");
         }
 
     }
-        static void outputBoard (String[][]board,int live){
-            String leftBlock = "| ";
-            String rightBlock = "|";
-            String wall = "+ —— + —— + —— + —— + —— +";
 
-            for (String[] raw : board) {
-                System.out.println(wall);
-                for (String col : raw) {
-                    System.out.print(leftBlock + col + " ");
-                }
-                System.out.println(rightBlock);
-            }
+    static void outputBoard(String[][] board, int live) {
+        String leftBlock = "| ";
+        String rightBlock = "|";
+        String wall = "+ —— + —— + —— + —— + —— +";
+
+        for (String[] raw : board) {
             System.out.println(wall);
-
-
-            System.out.println("Количество жизней:\t" + live + "\n");
+            for (String col : raw) {
+                System.out.print(leftBlock + col + " ");
+            }
+            System.out.println(rightBlock);
         }
-    }
+        System.out.println(wall);
+String health = "\uD83D\uDC96";
 
+        System.out.println(health + live + "\n");
+    }
+}
 
 
 
